6969fbb0f701498555e983a0fe13791fd6c9b9bc
Gabriel Silva <gabrielsilva2018automacao@gmail.com>
2025-06-14 14:33:07 -0300

    Projeto Barbearia

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   }
 
   function HandleModal(idmodal, abrir_fechar) {
-    if (idmodal == 1) {
+    if (idmodal === 1) {
       setIsModal1Open(abrir_fechar)
     } 
     else {
@@ -53,72 +53,82 @@ function DisableOption(optionTime, selectedDate) {
       <header>
         <nav>
           <ul>
-            <li><a href="#sobre">Sobre</a></li>
-            <li><a href="#servicos">Serviços</a></li>
+              <div class="container"> {/* classe para estilizar as tags superiores */}
+              <div class="sobre">
+                <li><a href="#sobre">Sobre</a></li>
+              </div>
+              
+              <div class="servicos">
+                <li><a href="#servicos">Serviços</a></li>
+              </div>
+            </div>
           </ul>
         </nav>
       </header>
 
-        <h1>SEU CORTE FALA POR VOCÊ ANTES MESMO DE VOCÊ DIZER UMA PALAVRA</h1>
-        <h2>Horário de funcionamento: 08h às 19h</h2>
+        <div id="inicial"> {/* ID para estilizar os parágrafos na página inicial */}
+          <h1>SEU CORTE FALA POR VOCÊ ANTES MESMO DE VOCÊ DIZER UMA PALAVRA</h1>
+          <h2>Horário de funcionamento: 08h às 19h</h2>
+        </div>
 
         <input type="button" value="Agendar serviço" onClick={() => HandleModal(1, true)} />
 
-        <Modal isOpen={isModal1Open} contentLabel="Agendar Serviço">
-          <h2>Agendar Serviço</h2>
-          <form>
-            <div>
-              <label>Nome:</label>
-              <input type="text" value={iptName} onChange={(e) => setIptName(e.target.value)}/>
-            </div>
-            <div>
-              <label>Data:</label>
-              <input type="date" min={FormattedToday()} value={iptDate} onChange={(e) => setIptDate(e.target.value)}/>
-            </div>
-            <div>
-              <label>Horário:</label>
-              <select value={inicialOption} onChange={(e) => setInicialOption(e.target.value)}> 
-                <option>Selecione um horário</option>
-                <option disabled={DisableOption("08:00", iptDate)}>08:00</option>
-                <option disabled={DisableOption("09:00", iptDate)}>09:00</option>
-                <option disabled={DisableOption("10:00", iptDate)}>10:00</option>
-                <option disabled={DisableOption("11:00", iptDate)}>11:00</option>
-                <option disabled={DisableOption("12:00", iptDate)}>12:00</option>
-                <option disabled={DisableOption("15:00", iptDate)}>15:00</option>
-                <option disabled={DisableOption("16:00", iptDate)}>16:00</option>
-                <option disabled={DisableOption("17:00", iptDate)}>17:00</option>
-                <option disabled={DisableOption("18:00", iptDate)}>18:00</option>
-                <option disabled={DisableOption("19:00", iptDate)}>19:00</option>
-              </select>
-            </div>
-            <button type="button" onClick={() => HandleModal(2, true)}>Confirmar</button>
-          </form>
-          <button onClick={() => HandleModal(1, false)}>Fechar</button>
-        </Modal>
 
-        <Modal isOpen={isModal2Open} contentLabel="Agendamento realizado!">
-
-          <h2>Agendamento realizado</h2>
-          <button type="button" onClick={() => HandleModal(2, false)}>Voltar</button>
-
-        </Modal>
+        
+          <Modal isOpen={isModal1Open} contentLabel="Agendar Serviço">
+            <h2>Agendar Serviço</h2>
+            <form>
+              <div>
+                <label>Nome:</label>
+                <input type="text" value={iptName} onChange={(e) => setIptName(e.target.value)}/>
+              </div>
+              <div>
+                <label>Data:</label>
+                <input type="date" min={FormattedToday()} value={iptDate} onChange={(e) => setIptDate(e.target.value)}/>
+              </div>
+              <div>
+                <label>Horário:</label>
+                <select value={inicialOption} onChange={(e) => setInicialOption(e.target.value)}>
+                  <option>Selecione um horário</option>
+                  <option disabled={DisableOption("08:00", iptDate)}>08:00</option>
+                  <option disabled={DisableOption("09:00", iptDate)}>09:00</option>
+                  <option disabled={DisableOption("10:00", iptDate)}>10:00</option>
+                  <option disabled={DisableOption("11:00", iptDate)}>11:00</option>
+                  <option disabled={DisableOption("12:00", iptDate)}>12:00</option>
+                  <option disabled={DisableOption("15:00", iptDate)}>15:00</option>
+                  <option disabled={DisableOption("16:00", iptDate)}>16:00</option>
+                  <option disabled={DisableOption("17:00", iptDate)}>17:00</option>
+                  <option disabled={DisableOption("18:00", iptDate)}>18:00</option>
+                  <option disabled={DisableOption("19:00", iptDate)}>19:00</option>
+                </select>
+              </div>
+              <button type="button" onClick={() => HandleModal(2, true)}>Confirmar</button>
+            </form>
+            <button onClick={() => HandleModal(1, false)}>Fechar</button>
+          </Modal>
+          <Modal isOpen={isModal2Open} contentLabel="Agendamento realizado!">
+            <h2>Agendamento realizado</h2>
+            <button type="button" onClick={() => HandleModal(2, false)}>Voltar</button>
+          </Modal>
+    
       </section>
 
       <section id="sobre">
-        <h1>Sobre</h1>
-        <p>No nosso estúdio, cada corte é mais do que estilo — é identidade. Trabalhamos para realçar a personalidade de cada cliente com técnica, atenção aos detalhes e um atendimento personalizado.</p>
-        <p>Com profissionais experientes e um ambiente acolhedor, buscamos sempre superar expectativas e transformar sua visita em uma experiência única. Aqui, você não vem só cortar o cabelo — vem renovar a confiança.</p>
+        <div id="borda"> {/* ID para melhorar a tag sobre */}
+          <h1>Sobre</h1>
+          <p>No nosso estúdio, cada corte é mais do que estilo — é identidade. Trabalhamos para realçar a personalidade de cada cliente com técnica, atenção aos detalhes e um atendimento personalizado.</p>
+          <p>Com profissionais experientes e um ambiente acolhedor, buscamos sempre superar expectativas e transformar sua visita em uma experiência única. Aqui, você não vem só cortar o cabelo — vem renovar a confiança.</p>
+        </div>
       </section>
 
       <section id="servicos">
-        <h1>Serviços</h1>
-        <p>Oferecemos cortes de cabelo e barba personalizados, com técnicas modernas e clássicas, garantindo um visual impecável e de qualidade.</p>
+          <h1>Serviços</h1>
+          <p>Oferecemos cortes de cabelo e barba personalizados, com técnicas modernas e clássicas, garantindo um visual impecável e de qualidade.</p>
+        
       </section>
     </main>
   )
 }
 
 export default App
-
-
 
